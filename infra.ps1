44c5602c404d0b39c029e75ece647adc7899bcdc
Naveen <naveenatnew@gmail.com>
2026-07-10 10:42:26 +0530

    Infra updates

--- infra.ps1
+++ infra.ps1
@@ -1,45 +1,54 @@
+# Run az login manually before running this script
+
+
 $rg = "mlDemo-rg"
 $region = "southindia"
-$AKS_NAME = "mlDemo-AKS"
+$aks = "mlDemo-AKS"
 $identity = "github-oidc-identity"
-# az login
+$repo = "repo:naveenatnew/azure-ai-mlops-demo:ref:refs/heads/main"
 
-# az group create `
-#     --name $rg `
-#     --location $region
+az group create `
+    --name $rg `
+    --location $region
 
-# az aks create `
-#     --resource-group $rg `
-#     --name $AKS_NAME `
-#     --tier free `
-#     --node-count 1 `
-#     --node-vm-size Standard_B2ms `
-#     --generate-ssh-keys
+az aks create `
+    --resource-group $rg `
+    --name $aks `
+    --tier free `
+    --node-count 1 `
+    --node-vm-size Standard_B2ms `
+    --generate-ssh-keys
 
-# az aks get-credentials `
-#     --resource-group $rg `
-#     --name $AKS_NAME `
-#     --overwrite-existing
+az aks get-credentials `
+    --resource-group $rg `
+    --name $aks `
+    --overwrite-existing
 
-# az identity create `
-#     --resource-group $rg `
-#     --name $identity
+az identity create `
+    --resource-group $rg `
+    --name $identity
 
-# $AKS_ID = az aks show `
-#     --resource-group $rg `
-#     --name $AKS_NAME `
-#     --query id `
-#     --output tsv
-# az role assignment create `
-#     --assignee 9040dbef-782b-4c9d-9944-d2cb61d9bca4 `
-#     --role "Azure Kubernetes Service Cluster User Role" `
-#     --scope $AKS_ID
+$aksId = az aks show `
+    --resource-group $rg `
+    --name $AKS_NAME `
+    --query id `
+    --output tsv
 
-# az identity federated-credential create `
-#     --resource-group $rg `
-#     --identity-name $identity `
-#     --name github-main `
-#     --issuer "https://token.actions.githubusercontent.com" `
-#     --subject "repo:naveenatnew/azure-ai-mlops-demo:ref:refs/heads/main" `
-#     --audiences "api://AzureADTokenExchange"
+$principalId =az identity show `
+    --name $identity `
+    --resource-group $rg `
+    --query principalId
+
+az role assignment create `
+    --assignee $principalId `
+    --role "Azure Kubernetes Service Cluster User Role" `
+    --scope $aksId
+
+az identity federated-credential create `
+    --resource-group $rg `
+    --identity-name $identity `
+    --name github-main `
+    --issuer "https://token.actions.githubusercontent.com" `
+    --subject $repo `
+    --audiences "api://AzureADTokenExchange"
 
